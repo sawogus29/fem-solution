6392923db481ea746444cf091d94aa2f59dacdc3
Jaehyeon Park <sawogus29@gmail.com>
2021-09-18 05:05:48 +0900

    fix bug

--- update-index.py
+++ update-index.py
@@ -2,6 +2,6 @@ def main():
     with open("index.html", mode="w") as fp:
         fp.write("<html><body><p>Hello Github Action</p><body></html")
     
-if __name__ == "main":
+if __name__ == "__main__":
     main()
     
