aef5d135681ba2c3415fbd552429ce96008073f3
Jaehyeon Park <sawogus29@gmail.com>
2021-09-18 05:09:20 +0900

    add log message

--- update-index.py
+++ update-index.py
@@ -1,6 +1,9 @@
 def main():
+    print("enter main")
     with open("index.html", mode="w") as fp:
         fp.write("<html><body><p>Hello Github Action</p><body></html")
+        print("writing file")
+    print("exit main")
     
 if __name__ == "__main__":
     main()
